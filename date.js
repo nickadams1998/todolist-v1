2c4dea080eb0ed12b9679e9398477643b6a97204
nickadams1998 <161159305+nickadams1998@users.noreply.github.com>
2024-05-07 17:22:32 -1000

    Update date.js

--- date.js
+++ date.js
@@ -1,14 +1,6 @@
-//jshint esversion: 6
-
-  //get the system date 
-
-module.exports.getDate = getDate;
-
-function getDate() {
-    
+module.exports.getDate = function() {
     let today = new Date();
   
-    // set display options for date - long is characters for weekday like Thursday, numeric is date in #
     let options = {
         weekday: "long",
         day: "numeric",
@@ -16,6 +8,6 @@ function getDate() {
         year: "numeric"
     };
     
-    // set variable day to use the options to display the day
     return today.toLocaleDateString("en-US", options);
+}s);
 }
